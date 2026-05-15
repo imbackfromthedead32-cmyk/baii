3ef6b28fcb3b2b6cfbad534d23f9069f1c370fdd
imbackfromthedead32-cmyk <imbackfromthedead32@gmail.com>
2026-05-15 18:02:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -739,7 +739,7 @@ const STATIC_BUNDLES = [
     id: "bundle_eyekonic",
     name: "EYEKONIC Bundle",
     rarity: "Icon",
-    imageUrl: "",
+    imageUrl: "https://cdn.discordapp.com/attachments/1504485556325715980/1504534511734755418/250701-eyekons-global-membership-is-officially-open-v0-wz97d3saEhaS8vrod4ixzNY0QxorUEbCSmRWHTpJiSg.jpg?ex=6a07ff34&is=6a06adb4&hm=1b7dda52f15c7f150c66f2e21185e371649d5457d70cb7aff616c2a0004dad92&",
     price: BUNDLE_PRICE,
     isBundle: true,
     skins: [
@@ -755,7 +755,7 @@ const STATIC_BUNDLES = [
     id: "bundle_pinkyup",
     name: "PINKY UP Bundle",
     rarity: "Icon",
-    imageUrl: "",
+    imageUrl: "https://cdn.discordapp.com/attachments/1504485556325715980/1504534511327772682/channels4_profile.jpg?ex=6a07ff34&is=6a06adb4&hm=ad77502395150f5fb7851507d58beff19a56bc3c357ff08fe881ae56c60132ed&",
     price: BUNDLE_PRICE,
     isBundle: true,
     skins: [
@@ -774,14 +774,14 @@ let _overwatchBundleCache = null;
 async function getOverwatchBundle() {
   if (_overwatchBundleCache) return _overwatchBundleCache;
   const skins = await fetchFortniteSkins();
-  const OW_TERMS = ["tracer","genji","kiriko","mercy","pharah","overwatch","dva","soldier:76","reaper","widowmaker","ana","hanzo","bastion","symmetra","winston","zarya","roadhog","junkrat","lucio","moira"];
+  const OW_TERMS = ["tracer","mercy","genji","d.va"];
   const owSkins = skins.filter(s => OW_TERMS.some(t => s.name.toLowerCase().includes(t)));
   const chosen = owSkins.length >= 3 ? owSkins.slice(0,6) : skins.filter(s => s.rarity.toLowerCase() === "epic").slice(0,6);
   _overwatchBundleCache = {
     id: "bundle_overwatch",
     name: "Overwatch Bundle",
     rarity: "Epic",
-    imageUrl: chosen[0]?.imageUrl ?? "",
+    imageUrl: "https://cdn.discordapp.com/attachments/1504485556325715980/1504535529474232391/fortnite-x-overwatch-all-skins-emotes-prices-full-showcase.jpg?ex=6a080027&is=6a06aea7&hm=053ee4016f3e9fe9a6d76caf5523140a0d1ea2bb899a50e150e7e8eb93a7716f&",
     price: BUNDLE_PRICE,
     isBundle: true,
     skins: chosen.map(s => ({ id: s.id, name: s.name, imageUrl: s.imageUrl })),
